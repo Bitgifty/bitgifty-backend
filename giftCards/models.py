e50149ee5c686accfc372bd45ab7e44f75b4cdd6
Princewill Olaiya <princewillolaiya@gmail.com>
2022-12-16 17:56:04 +0100

    added gitcard base model

--- giftCards/models.py
+++ giftCards/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class GiftCard(models.Model):
+    currency = models.CharField(max_length=255)
+    amount = models.FloatField(default=0.0)
+    quantity = models.IntegerField(default=0)
+    note = models.TextField(null=True, blank=True)
+    fees = models.FloatField(default=0.0)
+    binance_code = models.CharField(max_length=255, null=True, blank=True)
+    encrypted_code = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return self.currency
