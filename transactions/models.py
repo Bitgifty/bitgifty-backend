dd2580ad543f5b69abadf2ffe2f95e15398cf63c
Princewill Olaiya <princewillolaiya@gmail.com>
2022-12-16 18:02:10 +0100

    added transaction model

--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Transaction(models.Model):
+    wallet = models.ForeignKey('accounts.Account', on_delete=models.CASCADE)
+    amount = models.FloatField(default=0.0)
+    asset = models.CharField(max_length=255)
+    action = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.wallet.email
