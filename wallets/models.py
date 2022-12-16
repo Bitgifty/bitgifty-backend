62de20faef529048d90a7415525f280b14b5e8d9
Princewill Olaiya <princewillolaiya@gmail.com>
2022-12-16 17:49:07 +0100

    added base wallet model

--- wallets/models.py
+++ wallets/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Wallet(models.Model):
+    owner = models.ForeignKey('accounts.Account')
+    balance = models.FloatField(default=0.0)
+
+    def __str__(self):
+        return self.owner.email
+
+    def fund(self, amount):
+        self.balance += amount
+        self.save()
+    
+    def debit(self, amount):
+        self.balance -= amount
+        self.save()
