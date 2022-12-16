5aab3e1797d4f6604c0445cbb1bf439c0f0c36f1
Princewill Olaiya <princewillolaiya@gmail.com>
2022-12-16 18:03:50 +0100

    ran transactions migration

--- BinanceGift/settings.py
+++ BinanceGift/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'accounts.apps.AccountsConfig',
     'giftCards.apps.GiftcardsConfig',
+    'transactions.apps.TransactionsConfig',
     'wallets.apps.WalletsConfig',
 ]
 
