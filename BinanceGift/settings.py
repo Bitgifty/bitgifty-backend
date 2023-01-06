420bd7ef59269688b06f6a770315cf9f8d4d9492
Princewill Olaiya <princewillolaiya@gmail.com>
2023-01-06 16:44:28 +0100

    media and static url

--- BinanceGift/settings.py
+++ BinanceGift/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'djangowasthebestchoiceinbuildingthisplatform&thereisnothingyoucans
 # env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 DEBUG_PROPAGATE_EXCEPTIONS = True
 
@@ -177,6 +177,9 @@ STATIC_URL = '/static/'
 # ]
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # new
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- BinanceGift/urls.py
+++ BinanceGift/urls.py
@@ -20,6 +20,8 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.urls import re_path
+from django.conf.urls.static import static
+from BinanceGift import settings
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -49,4 +51,4 @@ urlpatterns = [
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
