427acb2eeedaa22d88470c547d186b29a191b3c5
Princewill Olaiya <princewillolaiya@gmail.com>
2023-01-20 16:58:49 +0100

    debug databse

--- BinanceGift/settings.py
+++ BinanceGift/settings.py
@@ -114,24 +114,24 @@ WSGI_APPLICATION = 'BinanceGift.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+# if DEBUG:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+# else:
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'aoxcamsp',
+        'USER': 'aoxcamsp',
+        'PASSWORD': '3IZJGbntcqWyrbUpfvyKmvR8Dus_b21Q',
+        'HOST': 'dumbo.db.elephantsql.com',
+        'PORT': '5432',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'aoxcamsp',
-            'USER': 'aoxcamsp',
-            'PASSWORD': '3IZJGbntcqWyrbUpfvyKmvR8Dus_b21Q',
-            'HOST': 'dumbo.db.elephantsql.com',
-            'PORT': '5432',
-        }
-    }
+}
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3030",
