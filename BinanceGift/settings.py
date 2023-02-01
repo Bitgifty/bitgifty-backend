a39b1220d04a44de432d4468e292147fa6350e79
Princewill Olaiya <princewillolaiya@gmail.com>
2023-02-01 19:26:07 +0100

    added new ip address to allowed host

--- BinanceGift/settings.py
+++ BinanceGift/settings.py
@@ -30,7 +30,7 @@ DEBUG = env("DEBUG")
 
 DEBUG_PROPAGATE_EXCEPTIONS = True
 
-ALLOWED_HOSTS = ['giftcarded.onrender.com', '127.0.0.1']
+ALLOWED_HOSTS = ['giftcarded.onrender.com', '127.0.0.1', '191.101.2.95']
 
 
 # Application definition
