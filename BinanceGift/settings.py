7532d3c0e05b6166d11af1a4565a2c7e09bd0811
Princewill Olaiya <princewillolaiya@gmail.com>
2023-03-07 11:51:56 +0100

    smtp mail trap

--- BinanceGift/settings.py
+++ BinanceGift/settings.py
@@ -253,5 +253,8 @@ LOGGING = {
     }
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'    
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = env('EMAIL_PORT')
