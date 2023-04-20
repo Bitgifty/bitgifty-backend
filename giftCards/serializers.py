fed88ab5f051172419d9d5a59ccedad4a9741a8b
Princewill Olaiya <princewillolaiya@gmail.com>
2023-04-20 02:35:10 +0100

    added fields to redeem serializer

--- giftCards/serializers.py
+++ giftCards/serializers.py
@@ -5,11 +5,12 @@ from .models import GiftCard, Redeem
 class GiftCardSerializer(serializers.ModelSerializer):
     class Meta:
         model = GiftCard
-        exclude = ["code", ]
+        fields = "__all__"
         read_only_fields = ["code", "encrypted_code", "account"]
 
 
 class RedeemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Redeem
+        fields = "__all__"
         read_only_fields = ["account",]
