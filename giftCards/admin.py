1c00d422659034751fc3e1c69826301582b7cb28
Princewill Olaiya <princewillolaiya@gmail.com>
2023-04-20 03:04:36 +0100

    clodinary images

--- giftCards/admin.py
+++ giftCards/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import GiftCard
+from .models import GiftCard, Redeem, GiftCardImage
 # Register your models here.
 
-admin.site.register(GiftCard)
+admin.site.register([GiftCard, Redeem, GiftCardImage])
