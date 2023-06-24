06cde2e22bef0829f662f08f74bfe8b911b69bdb
Princewill Olaiya <princewillolaiya@gmail.com>
2023-06-24 12:32:29 +0100

    wallet fix endpoint

--- wallets/urls.py
+++ wallets/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.WalletAPIView.as_view()),
+    path('fix', views.recreate_wallet)
 ]
--- wallets/views.py
+++ wallets/views.py
@@ -2,6 +2,8 @@ import os
 
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
@@ -10,6 +12,7 @@ from rest_framework.exceptions import ValidationError
 from .serializers import WalletSerializer
 from .models import Wallet
 
+from accounts.models import Account
 from core.utils import Blockchain
 # Create your views here.
 
@@ -49,3 +52,71 @@ class WalletAPIView(generics.GenericAPIView):
             return Response(wallet_list, status=status.HTTP_200_OK)
         except Exception as exception:
             raise ValidationError({"error": exception})
+
+
+def recreate_wallet(request):
+    TATUM_API_KEY = os.getenv("TATUM_API_KEY")
+    client = Blockchain(key=TATUM_API_KEY)
+    
+    cloud_name = os.getenv("CLOUD_NAME")
+    network_mapping = {
+        "bnb": "bnb",
+        "bitcoin": "bitcoin",
+    }
+    accounts = Account.objects.all()
+    for account in accounts:
+        for key in network_mapping:
+            if key == "bnb":
+                credentials = client.generate_bnb_wallet()
+                wallet_address = credentials["address"]
+                private_key = credentials["privateKey"]
+                encrypted_credentials = client.encrypt_credentials(
+                    private_key=private_key
+                )
+                private_enc = encrypted_credentials["private_key"]
+                xpub_enc = None
+                mnemonic_enc = None
+                try:
+                    old = Wallet.objects.get(owner=account, network=key.title())
+                    old.delete()
+                except Exception:
+                    pass
+            else:
+                credentials = client.generate_credentials(network_mapping[key])
+                wallet = client.generate_wallet(credentials["xpub"], network_mapping[key])
+                private_key = client.generate_private_key(credentials["mnemonic"], network_mapping[key])
+            
+                encrypted_credentials = client.encrypt_credentials(
+                credentials["mnemonic"], credentials["xpub"], private_key
+                )
+                wallet_address = wallet["address"]
+                
+                xpub_enc = encrypted_credentials["Xpub"]
+                mnemonic_enc = encrypted_credentials["Mnemonic"]
+                private_enc = encrypted_credentials["private_key"]
+                client.upload_qrcode(wallet_address, account.email)
+                try:
+                    old = Wallet.objects.get(owner=account, network=key.title())
+                    old.delete()
+                except Exception:
+                    pass
+            if xpub_enc and mnemonic_enc:
+                user_wallet = Wallet(
+                    owner=account,
+                    address=wallet_address,
+                    private_key=private_enc.decode(),
+                    xpub=xpub_enc.decode(),
+                    mnemonic=mnemonic_enc.decode(),
+                    network=key.title(),
+                    qrcode=f'https://res.cloudinary.com/{cloud_name}/image/upload/qr_code/{account.email}/{wallet_address}.png'
+                )
+            else:
+                user_wallet = Wallet(
+                    owner=account,
+                    address=wallet_address,
+                    private_key=private_enc.decode(),
+                    network=key.title(),
+                    qrcode=f'https://res.cloudinary.com/{cloud_name}/image/upload/qr_code/{account.email}/{wallet_address}.png'
+                )
+            user_wallet.save()
+    return HttpResponse("success")
