612e9cd5820ce4cc87339030a61374b428150fb8
Princewill Olaiya <princewillolaiya@gmail.com>
2023-07-28 19:28:48 +0100

    swap admin changes

--- swap/admin.py
+++ swap/admin.py
@@ -1,4 +1,22 @@
 from django.contrib import admin
-from .models import SwapTable, USDTNaira, USDTPrice
+from .models import SwapTable, USDTNaira, USDTPrice, Swap
 # Register your models here.
 admin.site.register([SwapTable, USDTNaira, USDTPrice])
+
+@admin.register(Swap)
+class SwapAdmin(admin.ModelAdmin):
+    list_display = ('swap_amount', 'swap_from_wallet', 'swap_to_wallet')
+
+    def swap_from_wallet(self, obj):
+        if obj.swap_from:
+            email = obj.swap_from.owner.email
+        else:
+            email = ""
+        return email
+    
+    def swap_to_wallet(self, obj):
+        if obj.swap_to:
+            email = obj.swap_to.owner.email
+        else:
+            email = ""
+        return email
